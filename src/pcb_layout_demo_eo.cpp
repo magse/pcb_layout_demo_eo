3b851c67e88b5b039126bf63939ffe5c5478a450
Magnus Sethson <magnus@sethson.se>
2021-04-06 14:56:56 +0200

    project setup

---
+++ src/pcb_layout_demo_eo.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  pcb_layout_demo_eo
+//
+//  Created by Magnus Sethson on 2021-04-06.
+//
+
+#include <iostream>
+
+#include "geometry2d.hpp"
+
+using namespace geometry2d;
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+	
+	geometry2d::geometry2d_test<double>();
+	
+	return 0;
+}
